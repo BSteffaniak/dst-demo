cfbbe802e9a2530e3a1f6c4768d925e248110baf
Braden Steffaniak <BradenSteffaniak@gmail.com>
2025-04-15 07:30:00 -0400

    Display simulated time & real time after execution

--- simulator/src/main.rs
+++ simulator/src/main.rs
@@ -2,13 +2,79 @@
 #![warn(clippy::all, clippy::pedantic, clippy::nursery, clippy::cargo)]
 #![allow(clippy::multiple_crate_versions)]
 
-use std::time::Duration;
+use std::{
+    sync::{LazyLock, atomic::AtomicU32},
+    time::Duration,
+};
 
 use dst_demo_server_simulator::{RNG, SIMULATOR_CANCELLATION_TOKEN, client, handle_actions, host};
 use dst_demo_simulator_harness::{
     turmoil::{self},
     utils::SEED,
 };
+
+static STEP: LazyLock<AtomicU32> = LazyLock::new(|| AtomicU32::new(1));
+
+pub trait TimeFormat {
+    fn into_formatted(self) -> String;
+}
+
+impl TimeFormat for u32 {
+    fn into_formatted(self) -> String {
+        u128::from(self).into_formatted()
+    }
+}
+
+impl TimeFormat for u64 {
+    fn into_formatted(self) -> String {
+        u128::from(self).into_formatted()
+    }
+}
+
+impl TimeFormat for u128 {
+    fn into_formatted(self) -> String {
+        #[must_use]
+        const fn plural(num: u128) -> &'static str {
+            if num == 1 { "" } else { "s" }
+        }
+
+        let years = self / 365 / 24 / 60 / 60 / 1000;
+        let days = self / 24 / 60 / 60 / 1000 % 365;
+        let hours = self / 60 / 60 / 1000 % 24;
+        let minutes = self / 60 / 1000 % 60;
+        let seconds = self / 1000 % 60;
+        let ms = self % 1000;
+
+        if years > 0 {
+            format!(
+                "{years} year{}, {days} day{}, {hours} hour{}, {minutes} minute{}, {seconds}s, {ms}ms",
+                plural(years),
+                plural(days),
+                plural(hours),
+                plural(minutes),
+            )
+        } else if days > 0 {
+            format!(
+                "{days} day{}, {hours} hour{}, {minutes} minute{}, {seconds}s, {ms}ms",
+                plural(days),
+                plural(hours),
+                plural(minutes),
+            )
+        } else if hours > 0 {
+            format!(
+                "{hours} hour{}, {minutes} minute{}, {seconds}s, {ms}ms",
+                plural(hours),
+                plural(minutes),
+            )
+        } else if minutes > 0 {
+            format!("{minutes} minute{}, {seconds}s, {ms}ms", plural(minutes))
+        } else if seconds > 0 {
+            format!("{seconds}s, {ms}ms")
+        } else {
+            format!("{ms}ms")
+        }
+    }
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     unsafe {
@@ -28,11 +94,23 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     pretty_env_logger::init();
 
+    let start = std::time::SystemTime::now();
+
     let resp = std::panic::catch_unwind(|| run_simulation(duration_secs));
+    let step = STEP.load(std::sync::atomic::Ordering::SeqCst);
+
+    let end = std::time::SystemTime::now();
+    let real_time_millis = end.duration_since(start).unwrap().as_millis();
 
     log::info!(
-        "Server simulator finished (seed={seed}) successful={}",
-        resp.as_ref().is_ok_and(Result::is_ok)
+        "Server simulator finished\n\
+        seed={seed}\n\
+        successful={successful}\n\
+        real_time_elapsed={real_time}\n\
+        simulated_time_elapsed={simulated_time}",
+        successful = resp.as_ref().is_ok_and(Result::is_ok),
+        real_time = real_time_millis.into_formatted(),
+        simulated_time = step.into_formatted(),
     );
 
     resp.unwrap()
@@ -49,9 +127,9 @@ fn run_simulation(duration_secs: u64) -> Result<(), Box<dyn std::error::Error>> 
     client::fault_injector::start(&mut sim);
     client::healer::start(&mut sim);
 
-    let mut step = 1;
-
     while !SIMULATOR_CANCELLATION_TOKEN.is_cancelled() {
+        let step = STEP.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
+
         if step % 1000 == 0 {
             #[allow(clippy::cast_precision_loss)]
             if duration_secs < u64::MAX {
@@ -78,8 +156,6 @@ fn run_simulation(duration_secs: u64) -> Result<(), Box<dyn std::error::Error>> 
                 return Err(e);
             }
         }
-
-        step += 1;
     }
 
     if !SIMULATOR_CANCELLATION_TOKEN.is_cancelled() {

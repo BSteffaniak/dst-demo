77fc2b956a97a6ed0ac75292fa35bd07c42de881
Braden Steffaniak <BradenSteffaniak@gmail.com>
2025-05-05 21:27:28 -0400

    Add thread_id to async lib

--- async/src/lib.rs
+++ async/src/lib.rs
@@ -2,6 +2,11 @@
 #![warn(clippy::all, clippy::pedantic, clippy::nursery, clippy::cargo)]
 #![allow(clippy::multiple_crate_versions)]
 
+use std::{
+    cell::RefCell,
+    sync::{LazyLock, atomic::AtomicU64},
+};
+
 #[cfg(feature = "macros")]
 pub use dst_demo_async_macros::{inject_yields, inject_yields_mod};
 #[cfg(any(feature = "tokio", feature = "simulator"))]
@@ -14,6 +19,17 @@ pub mod tokio;
 pub mod simulator;
 
 pub mod runtime;
+
+static THREAD_ID_COUNTER: LazyLock<AtomicU64> = LazyLock::new(|| AtomicU64::new(1));
+
+thread_local! {
+    static THREAD_ID: RefCell<u64> = RefCell::new(THREAD_ID_COUNTER.fetch_add(1, std::sync::atomic::Ordering::SeqCst));
+}
+
+#[must_use]
+pub fn thread_id() -> u64 {
+    THREAD_ID.with_borrow(|x| *x)
+}
 
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
